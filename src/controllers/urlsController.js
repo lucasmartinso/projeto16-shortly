d647856e1cd27b4aba00503202ccfc214caba338
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-03 16:26:08 -0300

    getUrlById ok

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -5,13 +5,47 @@ dotenv.config();
 
 export async function postUrl(req,res) { 
     const { url } = req.body;
-    const { authorization } = req.headers;
-    const tokenAuth = authorization?.replace("Bearer ", "");
     const shortUrl = nanoid(); 
     
     try {
         await connection.query('INSERT INTO urls ("shortUrl",url) VALUES ($1,$2)',[shortUrl,url]);
         return res.send({ shortUrl }).status(201);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}; 
+
+export async function getUrlbyId(req,res) { 
+    const { id } = req.params;
+    try {
+        const { rows: url } = await connection.query(`
+            SELECT id,"shortUrl",url 
+            FROM urls 
+            WHERE id= $1`,[id]);
+        if(url.length === 0) { 
+            return res.sendStatus(404);
+        }
+        return res.send(url).status(200);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    } 
+};
+
+export async function getUrlbyShortUrl(req,res) { 
+    const { shortUrl } = req.params;
+    console.log(shortUrl); 
+
+    try { 
+        await connection.query('INSERT INTO visits ("urlId","userId") VALUES ($1,$2)',[])
+        const { rows: visits } = await connection.query(`
+            SELECT urls.id, urls."shortUrl",urls.url, SUM(visits."urlId") AS visits,
+            FROM urls 
+            JOIN visits ON urls.id = visits."urlId" 
+            GROUP BY visits."urlId"
+        `);
+        return res.send(visits).status(200);
     } catch (error) {
         console.log(error);
         return res.sendStatus(500);
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import dotenv from "dotenv"; 
-import { postUrl }  from "../controllers/urlsController.js"
-import validateUrl from "../middlewares/validateUrlMiddleware.js"
+import { postUrl, getUrlbyId, getUrlbyShortUrl }  from "../controllers/urlsController.js"
+import validateUrl from "../middlewares/validateUrlMiddleware.js";
+import { validateAuth } from "../middlewares/validateAuthMiddleware.js";
 dotenv.config(); 
 
 const router = Router();
 
-router.post("/urls/shorten", validateUrl,postUrl);
-router.get("/urls/:id",); 
-router.get("/urls/open/:shortUrl",);  
+router.post("/urls/shorten", validateAuth,validateUrl,postUrl);
+router.get("/urls/:id", getUrlbyId); 
+router.get("/urls/open/:shortUrl", getUrlbyShortUrl);  
 router.delete("/urls/:id",);
 
 export default router;
