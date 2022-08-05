8f04e59f39871677abd67255b074e88f62b06ed4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-05 15:12:42 -0300

    Prepare for deploy

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,21 +8,16 @@ export async function getHistoric(req,res) {
     try {
         const { rows: historic } = await connection.query(`SELECT json_build_object(
             'id', users.id,
-            'name', users.name,
-            'shortenedUrls', json_build_object(
-                'id', urls.id, 
-                'shortUrl', urls."shortUrl",
-                'url', urls.url
-                'visitCount', COUNT(v."usersId")
-                )
-            ) 
+            'name', users.name, 
+            'shortenedUrls' , (SELECT id,"shortUrl", url, json_agg(row_to_json("urls")) FROM "urls")
+            )
             FROM users 
             JOIN tokens t ON t."userId" = users.id
             JOIN "urlsUsers" uu ON users.id = uu."userId"
             JOIN urls ON uu."urlId" = urls.id
             JOIN visits v ON v."urlId" = urls.id
             WHERE t."userId" = $1
-            GROUP BY 
+            GROUP BY "urls.id", users.id
             `,[userId[0].userId]);
         return res.send(historic.map(hist => hist.json_build_object)).status(200);
     } catch (error) {
