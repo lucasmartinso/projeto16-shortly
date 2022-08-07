a49074349283904a6ffd19404147ef74f7ce49d5
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-07 16:12:29 -0300

    rote users/me finish

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -12,6 +12,11 @@ export async function postUrl(req,res) {
         await connection.query('INSERT INTO urls ("shortUrl",url) VALUES ($1,$2)',[shortUrl,url]); 
         const { rows: urlInfo } = await connection.query('SELECT * FROM urls WHERE url= $1',[url]);
         await connection.query('INSERT INTO "urlsUsers" ("urlId","userId") VALUES ($1,$2)',[urlInfo[0].id,userId[0].userId]);
+        const { rows: urlId } = await connection.query('SELECT id,url FROM urls WHERE "shortUrl"= $1',[shortUrl]);
+        const { rows: verifyExistence } = await connection.query('SELECT * FROM visits WHERE "urlId"= $1',[urlId[0].id]);
+        if(verifyExistence.length === 0) { 
+            await connection.query('INSERT INTO visits ("urlId") VALUES ($1)',[urlId[0].id]);
+        }
         return res.send({ shortUrl }).status(201);
     } catch (error) {
         console.log(error);
@@ -42,22 +47,18 @@ export async function getUrlbyShortUrl(req,res) {
     try { 
         const { rows: urlId } = await connection.query('SELECT id,url FROM urls WHERE "shortUrl"= $1',[shortUrl]);
         const { rows: verifyExistence } = await connection.query('SELECT * FROM visits WHERE "urlId"= $1',[urlId[0].id]);
-        if(verifyExistence.length === 0) { 
-            await connection.query('INSERT INTO visits ("urlId") VALUES ($1)',[urlId[0].id]);
-        } else {
-            await connection.query(`
-                UPDATE visits 
-                SET "countVisits"= $1 
-                WHERE "urlId"= $2
+        await connection.query(`
+            UPDATE visits 
+            SET "countVisits"= $1 
+            WHERE "urlId"= $2
             `,[verifyExistence[0].countVisits +1,urlId[0].id]);
-            const { rows: visits } = await connection.query(`
-                SELECT u.id, u."shortUrl", u.url, v."countVisits"
-                FROM urls u 
-                JOIN visits v ON v."urlId" = u.id
-                WHERE v."urlId" = $1
-                `,[urlId[0].id]);
-            console.log(visits);
-        }
+        const { rows: visits } = await connection.query(`
+            SELECT u.id, u."shortUrl", u.url, v."countVisits"
+            FROM urls u 
+            JOIN visits v ON v."urlId" = u.id
+            WHERE v."urlId" = $1
+            `,[urlId[0].id]);
+        console.log(visits);
         return res.redirect(200,urlId[0].url);
     } catch (error) {
         console.log(error);
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,16 +8,20 @@ export async function getHistoric(req,res) {
     try {
         const { rows: historic } = await connection.query(`SELECT json_build_object(
             'id', users.id,
-            'name', users.name, 
-            'shortenedUrls' , (SELECT id,"shortUrl", url, json_agg(row_to_json("urls")) FROM "urls")
-            )
-            FROM users 
-            JOIN tokens t ON t."userId" = users.id
-            JOIN "urlsUsers" uu ON users.id = uu."userId"
+            'name', users.name,
+            'visitCount', SUM(v."countVisits"), 
+            'shortenedUrls', json_agg(json_build_object(
+                'id', urls.id,
+                'shortUrl', urls."shortUrl",
+                'url', urls.url, 
+                'visitCount', v."countVisits"
+            )))
+            FROM users  
+            JOIN "urlsUsers" uu ON users.id= uu."userId"
             JOIN urls ON uu."urlId" = urls.id
             JOIN visits v ON v."urlId" = urls.id
-            WHERE t."userId" = $1
-            GROUP BY "urls.id", users.id
+            WHERE users.id= $1
+            GROUP BY users.id
             `,[userId[0].userId]);
         return res.send(historic.map(hist => hist.json_build_object)).status(200);
     } catch (error) {
