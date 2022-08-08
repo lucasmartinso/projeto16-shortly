7f4510191ce1e89332d26b0b59e5e60b59a98a75
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-08-08 16:01:07 -0300

    dump ok

--- src/schemas/signupSchema.js
+++ src/schemas/signupSchema.js
@@ -4,7 +4,7 @@ const signupSchema = joi.object({
     name: joi.string().required(), 
     email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     password: joi.string().required(),
-    repeatPassword: joi.string().required()
+    confirmPassword: joi.string().required()
 });
 
 export default signupSchema;
